6fd0dce568eb5e5f0bea88efd827833dc2f8ad7a
[Dan <123705636+dkopac@users.noreply.github.com>
2023-05-08 18:06:11 -0400

    updates

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-//Dependencies
 const express = require("express");
-const apiRoutes = require("./routes/apiRoutes.js");
-const htmlRoutes = require("./routes/htmlRoutes.js");
-
 const app = express();
 const PORT = process.env.PORT || 8080;
+const htmlRoutes = require("./routes/htmlRoutes.js");
+const apiRoutes = require("./routes/apiRoutes.js");
 
+//Parse JSON using Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/index.html"))
+);
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}.`));
