ade719d46708bfc69a81435b620f0a5c1811d403
[Dan <123705636+dkopac@users.noreply.github.com>
2023-05-08 22:50:07 -0400

    fixed index.js that caused all my errors

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -26,27 +26,24 @@ const hide = (elem) => {
 let activeNote = {};
 
 const getNotes = () =>
-  fetch("/notes", {
+  fetch("/api/notes", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
     },
   });
 
-const saveNote = (note) => {
-  console.log(note);
-
-  fetch("/notes", {
+const saveNote = (note) =>
+  fetch("/api/notes", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(note),
   });
-};
 
 const deleteNote = (id) =>
-  fetch(`notes/${id}`, {
+  fetch(`/api/notes/${id}`, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
